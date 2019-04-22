6ca79f0f43879009995826095f317857d8c084b9
ANIRBAN DEY <want2meetani@yahoo.in>
2019-04-22 20:56:16 +0530

    Add files via upload

---
+++ Explore weather trends REDME.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Explore-Weather-Trends
+
+# ***Summary***
+
+# In this project, I analyzed local and global temperature data and compare the temperature trends for the closest city from where I live which is 'Delhi' to overall global temperature trends.
+# 
+
+# The goal is to create a visualization and prepare a write up describing the similarities and differences between global temperature trends and temperature trends in the closest big city to where I live.
+
+# ***Introduction***
+
+# SQL Query was used to download (CSV) file that contains yearly average temperature of the City ‘Delhi’ and the global temperature.I have taken out the city and the global data and plotted the line graphs individually and then I had made the normalization by years and then I have made the comparison by means of Calculating the Moving Average (Rolling Average) w.r.t graph through scatter  and line graphs between  city and global temperature.
+
+# ***Project Outline***
+
+# Extract the data :
+#     
+# • Downloading 2 data files from SQL database as CSV.
+# 
+# Create a line chart :
+# 
+# • Calculating the Moving Average (Rolling Average) to make it easier to observe the trends when it be shown in Charts.
+
+# ***Submission***
+
+# The final analysis will take the form of a PDF document which contains a plot of the data, analysis of said plot and outlines the steps taken:
+
+# SQL Query was used to download (CSV) file that contains yearly average temperature of the City ‘Delhi’ and the global temperature.I have taken out the city and the global data and plotted the line graphs individually and then I had made the normalization by years and then I have made the comparison by means of Calculating the Moving Average (Rolling Average) w.r.t graph through scatter and line graphs between city and global temperature.
+# 
+# The toolss which I have used are Excel and SQL.
+# Then I have calculated 7 years moving average  & along with four neccessary observations as shown in the pdf file .
+# 
+
+# I made observations the i saw related to the data "you can find more details on the Report file" & I made a conclusion about the data.
